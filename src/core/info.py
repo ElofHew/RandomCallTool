70d671d600040ddec67d68e273ef05cffe02bb38
ElofHew <2352235176@qq.com>
2026-09-13 12:55:42 +0800

    V2.6

--- src/core/info.py
+++ src/core/info.py
@@ -26,10 +26,10 @@ official_website = "https://rct.danevan.top"
 
 # ── 主程序 RandomCallTool ──
 rct_appname = "RandomCallTool"
-rct_version = "2.5.3.1-beta2"
-rct_vercode = 253032
+rct_version = "2.6"
+rct_vercode = 260000
 rct_author = "Dan_Evan"
-rct_date = "2026-09-06"
+rct_date = "2026-09-13"
 rct_description = "一个基于Python + tkinter的随机抽取工具，支持随机抽组和随机抽人。"
 rct_prog_data_path = app_prog_data_path
 rct_log_path = os.path.join(rct_prog_data_path, "log")
